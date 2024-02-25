14b115f01b5a7fc259fc006c3936bd4036a51bbb
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2024-02-25 20:31:51 +0530

    Initial Commit

---
+++ Twitter-Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter-Clone
+//
+//  Created by Akhil on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
